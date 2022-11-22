db8c185bb5a15a0933dcf0e67fd1883a2c5ed47f
jaylon.cheng <jaylon.cheng@peliplat.com>
2022-11-22 10:54:24 +0800

    test JavaVTKActivity

--- 3DApp/src/main/java/tech/xx/a3dviewer/MainActivity.java
+++ 3DApp/src/main/java/tech/xx/a3dviewer/MainActivity.java
@@ -2,13 +2,28 @@ package tech.xx.a3dviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTest = findViewById(R.id.btn_test);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ 3DApp/src/main/java/tech/xx/a3dviewer/TestActivity.java
@@ -0,0 +1,19 @@
+package tech.xx.a3dviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+import com.kitware.JavaVTK.JavaVTKActivity;
+
+public class TestActivity extends JavaVTKActivity {
+
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_test);
+//    }
+}
